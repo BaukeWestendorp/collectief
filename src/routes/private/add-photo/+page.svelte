9d4b5b63e83423f893d0e15fbdbfba27d662d614
Bauke Westendorp <baukewestendorp@gmail.com>
2025-01-07 22:08:12 +0100

    Add base style to /privagte/add-photo

--- src/routes/private/add-photo/+page.svelte
+++ src/routes/private/add-photo/+page.svelte
@@ -5,7 +5,7 @@
 <form method="POST" action="/api/photo" enctype="multipart/form-data" use:enhance>
 	<label>
 		Title
-		<input name="title" type="text" />
+		<input name="title" type="text" required />
 	</label>
 	<label>
 		Alt-text
@@ -13,20 +13,29 @@
 	</label>
 	<label>
 		Date
-		<input name="date" type="date" />
+		<input name="date" type="date" required />
 	</label>
 	<label>
 		Photo
-		<input name="file" type="file" accept=".jpg,.jpeg" />
+		<input name="file" type="file" accept=".jpg,.jpeg" required />
 	</label>
 	<button type="submit">Upload</button>
 </form>
 
 <style>
 	form {
+		width: 100%;
+		max-width: 24rem;
+
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
 		gap: 1em;
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
 	}
 </style>
